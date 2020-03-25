628377a06232be75b7b0235f2bc5c96d27dacecc
bob2319 <30457588+bob2319@users.noreply.github.com>
2020-03-26 00:37:38 +0200

    Add files via upload

--- travelcream/inc/home_page/home-page-input.php
+++ travelcream/inc/home_page/home-page-input.php
@@ -2,7 +2,7 @@
 <form action="<?php echo admin_url('admin-ajax.php?action=send_mail'); ?>" class='header-form' role="search" method="get" id="searchform" action="">
 <input class='req-field' type="text" value="Where a you leaving from?" name="town" id="town" />
 <input class='req-field' type="text" value="Wgere do you want to go?" name="trip" id="trip" />
-<input type="date" placeholder='Data' value="Data" name="date" id="date" />
+<input placeholder="Date" class="textbox-n" type="text" onfocus="(this.type='date')"  id="date" />
 <input type="submit"  id="searchsubmit" value="Search" />
 </form>
 </div>
